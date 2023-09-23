07b115fd31f3c285519ae84d0fea37a771ee766a
ceeyIT <104657912+ceeyIT@users.noreply.github.com>
2023-09-23 18:29:55 -0500

    Create CalculatorTest.java

---
+++ CalculatorTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(15, calculator.multiply(5, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
+    }
+}
